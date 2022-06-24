b4422e3457aa5f0a37e0976fbb0952e80480aa71
tom <yotomtom57@gmail.com>
2022-06-24 13:49:58 +0900

    注文詳細コントローラー作成

--- app/Http/Controllers/OrderDetailController.php
+++ app/Http/Controllers/OrderDetailController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;
+
 
 class OrderDetailController extends Controller
 {
     //
     public function index(){
-        return view('order_detail');
+        $orderDetail = Order::find(1);
+        return view('order_detail',['orderDetail' => $orderDetail]);
     }
 }
